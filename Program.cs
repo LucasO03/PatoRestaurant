de02a9fc9bc437a5d1872c7fc5a9ce9af675a955
Lucas Santos de Oliveira <lucas.santos003@outlook.com>
2022-11-09 20:38:27 -0300

    Adicionado o login da área administrativa e seu controller

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => 
